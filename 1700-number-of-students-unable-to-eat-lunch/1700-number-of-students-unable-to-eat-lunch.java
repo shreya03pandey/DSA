40a7a819e18c034fe9cd12568b9fc3ff6db6bfc7
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2022-07-09 23:42:28 +0530

    Time: 3 ms (42.23%), Space: 42.2 MB (43.79%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countStudents(int[] students, int[] san) {
+        Queue<Integer>q=new LinkedList<Integer>();
+        for(int x:students)
+            q.add(x);
+        int i=0;
+        int cnt=0;
+        while(!q.isEmpty()&&cnt!=q.size())
+        {
+            if(q.peek()==san[i])
+            {
+                i++;
+                q.poll();
+                cnt=0;
+            }
+            else
+            {
+                q.add(q.poll());
+                cnt++;
+            }
+        }
+        return q.size();
+    }
+}
