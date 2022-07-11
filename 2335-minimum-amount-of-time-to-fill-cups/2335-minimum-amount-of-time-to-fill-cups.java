1bf576f4c6ce248d4fb167f237a355f2532cccde
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2022-07-11 15:31:11 +0530

    Time: 2 ms (50.00%), Space: 41.4 MB (80.00%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -1,23 +1,12 @@
 class Solution {
     public int fillCups(int[] amt) {
-        int cnt=0;
-      PriorityQueue<Integer>pq=new PriorityQueue<Integer>(Collections.reverseOrder());
+        int mx=0;
+        int sum=0;
         for(int x:amt)
-            if(x>0)
-                pq.add(x);
-        while(!pq.isEmpty())
         {
-            int f=pq.poll();
-            if(pq.size()>=1)
-            {
-                int s=pq.poll();
-                if(s-1>0)
-                pq.add(s-1);
-            }
-            if(f-1>0)
-            pq.add(f-1);
-            cnt++;
+            sum+=x;
+            mx=Math.max(mx,x);
         }
-        return cnt;
+        return  Math.max(mx,(int)Math.ceil(sum*0.5));
     }
 }
