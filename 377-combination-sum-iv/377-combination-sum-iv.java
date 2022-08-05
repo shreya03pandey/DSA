75c34d020781ceefb679482d8220f881acafd8ec
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2022-08-05 15:59:22 +0530

    Time: 3 ms (36.49%), Space: 42.6 MB (5.59%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int dp[][]=new int[nums.length][target+1];
+        for(int i=0;i<nums.length;i++)
+        Arrays.fill(dp[i],-1);
+       return solve( nums,target,0,nums.length,dp); 
+    }
+    public int solve(int[] nums,int tar,int idx,int n,int[][] dp)
+    {
+        if(tar==0)
+            return 1;
+        if(idx==n)
+            return 0;
+          if(dp[idx][tar]!=-1)
+              return dp[idx][tar];
+        
+        int pick=0;
+        if(nums[idx]<=tar)
+        pick=solve(nums,tar-nums[idx],0,n,dp);
+        int nopick=solve(nums,tar,idx+1,n,dp);
+        return dp[idx][tar]=pick+nopick;
+    }
+}
