cf4c129e24be83d4b7dca44d266e8350bf619d35
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2022-08-15 15:03:57 +0530

    Time: 10 ms (77.37%), Space: 45.2 MB (16.97%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,39 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       List<Integer>al=new ArrayList<>();
+      List<Integer>al=new ArrayList<>();
+        int n =nums.length;
+        int res=1;
         al.add(nums[0]);
-        int len=1;
-        int n=nums.length;
         for(int i=1;i<n;i++)
         {
-          if(nums[i]>al.get(al.size()-1))
-          {
-              al.add(nums[i]);
-              len++;
-          }
+            if(nums[i]>al.get(al.size()-1))
+            {
+               al.add(nums[i]);
+                res++;
+            }
             else
             {
-                int idx=binarySearch(al,nums[i]);
+                int idx=binary(al,nums[i]);
                 al.set(idx,nums[i]);
             }
         }
-        return len;
+        return res;
     }
-     
-      public int binarySearch(List<Integer>al,int tar)
-      {
-          int l=0,r=al.size()-1;
-          while(l<=r)
-          {
-              int mid=(l+r)/2;
-              if(al.get(mid)==tar)
-                  return mid;
-              if(al.get(mid)<tar)
-                  l=mid+1;
-              else
-                  r=mid-1;
-          }
-          return r+1;
-      }
-   
+    public int binary(List<Integer>al,int tar)
+    {
+        int l=0,r=al.size()-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(al.get(mid)<tar)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return r+1;
+    }
 }
