c63a6978c4ec611134c9625d83d569f8e75542ad
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2022-08-21 21:14:07 +0530

    Time: 2 ms (89.53%), Space: 46.2 MB (35.56%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -2,22 +2,22 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-       int dp[][]=new int[n][m];
-      for(int row[]: dp)
-      Arrays.fill(row,-1);
-       return isMin(dp,grid,n-1,m-1);
+       int dp[][]=new int[n][m]; 
+        for(int x[]:dp)
+            Arrays.fill(x,-1);
+      return isMin(dp,grid,n-1,m-1);  
     }
-    public int isMin(int[][] dp,int[][] grid,int i,int j)
+    public int isMin(int[][] dp ,int[][] grid,int i,int j)
     {
-        if(i==0 && j == 0)
-    return grid[0][0];
-  if(i<0 || j<0)
-    return (int)Math.pow(10,9);
-    if(dp[i][j]!=-1) return dp[i][j];
-    
-   int up = grid[i][j]+isMin(dp,grid,i-1,j);
-   int left = grid[i][j]+isMin(dp,grid,i,j-1);
-  
-   return dp[i][j] = Math.min(up,left);
+        if(i==0&&j==0)
+            return grid[0][0];
+        if(i<0||j<0)
+            return (int)Math.pow(10,9);
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+         int up=grid[i][j]+isMin(dp,grid,i-1,j);
+         int left=grid[i][j]+isMin(dp,grid,i,j-1);
+        return dp[i][j]= Math.min(up,left);
     }
 }
