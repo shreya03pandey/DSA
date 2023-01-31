ec1a3bf682af26415a0f69940dcff417837dd967
Shreya Pandey <90647101+shreya03pandey@users.noreply.github.com>
2023-02-01 00:15:51 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = S[j].charAt(0);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.numIslands(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the number of islands.
+    public int numIslands(char[][] grid) {
+        // Code here
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1'&& vis[i][j]==0)
+                {
+                dfs(grid,vis,i,j,n,m);
+                cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    public void dfs(char[][] grid,int[][] vis,int a,int b,int n,int m)
+    {
+        vis[a][b]=1;
+        int dx[]={-1,-1,-1,0,0,1,1,1};
+        int dy[]={-1,0,1,-1,1,-1,0,1};
+        for(int i=0;i<8;i++)
+        {
+            int x=a+dx[i];
+            int y=b+dy[i];
+            if(x>=0&&x<n&&y>=0&&y<m&&grid[x][y]=='1'&&vis[x][y]==0)
+            dfs(grid,vis,x,y,n,m);
+        }
+    }
+}
